4f938fdf176d16950623707935e8cedfc43e63c5
ahmedss1997 <asami6596@gmail.com>
2024-05-02 19:51:54 +0300

    first commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,112 +1,103 @@
-import Image from "next/image";
+"use client";
+
+import Navbar from "@/components/nav";
+import { useState } from "react";
+import { BsSliders } from "react-icons/bs";
+import { FaSearch, FaUserAlt } from "react-icons/fa";
+// import Image from "next/image";
 
 export default function Home() {
+  const [data, setData] = useState([] as Category[]);
+  const getData = async () => {
+    if (data.length > 0) {
+      return;
+    }
+    const res = await fetch(
+      "https://ymtaz.sa/api/client/digital-guide/categories"
+    );
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    } else {
+      const response = await res.json();
+      if (response.data) {
+        setData(response.data.categories);
+      }
+    }
+    console.log(data);
+  };
+  getData();
+
+  const [hoveredIndex, setHoveredIndex] = useState(-1);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <div className="">
+        <Navbar />
+        <div className="content-home text-center my-10">
+          <h1 className="text-3xl font-bold text-[#252525]">الدليل الرقمى</h1>
+          {/* Search Input */}
+          <div className="max-w-4xl mx-auto my-5 px-4">
+            <div className="relative flex-1">
+              <div className="absolute inset-y-0 left-0 flex items-center pl-7 pointer-events-none">
+                <BsSliders className="text-gray-400 h-5 w-5" />
+              </div>
+              <input
+                type="text"
+                placeholder="Search..."
+                className="w-full bg-[#2D4768] text-white py-2 px-3 rounded-md pl-16 focus:outline-none focus:ring-2 focus:ring-blue-500"
+              />
+              <div className="absolute inset-y-0 right-0 flex items-center pr-3 pointer-events-none">
+                <FaSearch className="text-gray-400 h-5 w-5" />
+              </div>
+            </div>
+          </div>
+          {/* Categories Box */}
+          <div className="container mx-auto my-10 p-3">
+            <div className="row-all flex flex-wrap">
+              {data.map((item, index) => (
+                <div
+                  key={index}
+                  className="col-item  p-3 xl:max-w-[33%] xl:basis-2/6 lg:max-w-[50%] lg:basis-3/6 max-w-full basis-full "
+                >
+                  <div
+                    className="box-home relative cursor-pointer shadow-[0_0px_4px_1px] shadow-[#1018281A] hover:text-white rounded-2xl text-center py-3 mx-4 basis-1/3"
+                    onMouseEnter={() => setHoveredIndex(index)}
+                    onMouseLeave={() => setHoveredIndex(-1)}
+                  >
+                    {/* Overlay with transition */}
+                    <div
+                      className={`absolute inset-0 bg-gradient-to-r from-[#CCA95A] to-[#8f763f] rounded-2xl text-center transition-opacity duration-700 opacity-0 ${
+                        hoveredIndex === index ? "opacity-100" : ""
+                      }`}
+                    ></div>
+                    <div className="content-box relative ">
+                      <div
+                        className={`icon-box shadow-[0_5px_30px_0px] flex relative justify-center items-center rounded-full w-32 h-32 mx-auto my-4 ${
+                          hoveredIndex === index
+                            ? "shadow-[#DDB762] !important bg-bg-gradient-to-r from-[#CCA95A] to-[#8f763f]"
+                            : "shadow-[#F9DAD5] bg-white"
+                        }`}
+                      >
+                        <FaUserAlt
+                          className={`text-[#DDB762] text-4xl z-50 ${
+                            hoveredIndex === index ? "text-white" : ""
+                          }`}
+                        />
+                      </div>
+                      <h2 className="text-2xl font-medium my-3">
+                        {item.title}
+                      </h2>
+                      <p className="text-sm mb-3">
+                        متوفر عدد {item.lawyers_count}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
       </div>
     </main>
   );
---
+++ src/components/nav.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { FaBars } from "react-icons/fa";
+
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  useEffect(() => {
+    const checkScreenSize = () => {
+      setIsSmallScreen(window.innerWidth < 1024);
+    };
+    checkScreenSize();
+    window.addEventListener("resize", checkScreenSize);
+    return () => window.removeEventListener("resize", checkScreenSize);
+  }, []);
+
+  useEffect(() => {
+    setShowMenu(false);
+  }, [isSmallScreen]);
+
+  const items = [
+    "الرئيسية",
+    "من نحن",
+    "اتصل بنا",
+    "الدعم الفنى",
+    "أرقام التشغيل",
+    "سياسة الخصوصية",
+  ];
+
+  return (
+    <nav className="py-4">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex justify-between items-center">
+        {/* Logo */}
+        <div className="flex-shrink-0">
+          <Image
+            src="/logo.png"
+            className=""
+            width={140}
+            height={70}
+            alt="logo"
+            priority
+          />
+        </div>
+
+        {/* List */}
+        <ul className={`hidden lg:flex text-white`} dir="rtl">
+          {items.map((item, index) => (
+            <li
+              key={index}
+              className="text-[#2D4768] mx-4 cursor-pointer"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+
+        {/* Bar icon */}
+        <div className="flex items-center">
+          {/* button on lg */}
+          <button className="text-[#2D4768]">
+            <FaBars className={`hidden lg:inline text-2xl cursor-pointer `} />
+          </button>
+          {/* button on sm */}
+          <button className="text-[#2D4768]" onClick={toggleMenu}>
+            <FaBars className={`lg:hidden text-2xl cursor-pointer `} />
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile Menu */}
+      {isSmallScreen && showMenu && (
+        <div
+          className={`lg:hidden px-3`}
+          style={{ transition: "transform 0.5s ease" }}
+        >
+          <ul className="text-white" dir="rtl">
+            {items.map((item, index) => (
+              <li
+                key={index}
+                className="py-2 px-4 text-[#2D4768] hover:bg-slate-50 cursor-pointer"
+              >
+                {item}
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+    </nav>
+  );
+};
+
+export default Navbar;
